d10118b31ecf39f3bdb179b26ae433331a8adcd0
Vishal Ravi <vishalravi2020@gmail.com>
2024-05-08 16:48:27 -0700

    moved old files into an appropriate new folder

---
+++ old/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 16:02:50 2024
+
+@author: visha
+"""
+
+import numpy
+import matplotlib
+import scipy
+import rocketcea 
+import CoolProp
+import sklearn
